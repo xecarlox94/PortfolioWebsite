561f23c09c61dd97f6c8a55c02a81ad0f938a2f1
xecarlox94 <jf94.uk@gmail.com>
2020-05-16 23:41:14 +0100

    project tile

--- src/components/fragments/homePage/projects.js
+++ src/components/fragments/homePage/projects.js
@@ -23,13 +23,40 @@ const projectsArr = [
   },
 ]
 
-const ProjectTile = ({ project }) => {
-  return (
-    <div>
-      <h1>{project.title}</h1>
-      <p>{project.description}</p>
-    </div>
-  )
+class ProjectTile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { mouseOver: false }
+  }
+
+  onHover = () => this.setState({ mouseOver: true })
+
+  onHoverOut = () => this.setState({ mouseOver: false })
+
+  render() {
+    const { project } = this.props
+
+    let overlayClasses = "z-10 bg-gray-300 w-full h-full bg-opacity-50 "
+
+    if (!this.state.mouseOver) overlayClasses += "invisible"
+
+    return (
+      <div
+        className="bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80)",
+        }}
+        onMouseOver={() => this.onHover()}
+        onMouseLeave={() => this.onHoverOut()}
+      >
+        <div className={overlayClasses}>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 const Projects = () => {
