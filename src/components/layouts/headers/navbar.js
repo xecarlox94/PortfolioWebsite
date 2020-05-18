82ef168465c3b2c5aee4fa9ba122196f240eb8ce
xecarlox94 <jf94.uk@gmail.com>
2020-05-18 18:06:46 +0100

    navbar refactored

--- src/components/layouts/headers/navbar.js
+++ src/components/layouts/headers/navbar.js
@@ -29,23 +29,23 @@ const HomeLink = () => {
   return <Link to="/">{site.siteMetadata.title}</Link>
 }
 
-class MenuBurger extends React.Component {
-  render() {
-    return (
-      <svg
-        onClick={this.props.menuOpen}
-        style={{ fill: "white", cursor: "pointer" }}
-        height="25"
-        width="25"
-        viewBox="0 0 128 128"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M64 128a64 64 0 1164-64 64 64 0 01-64 64zM64 4a60 60 0 1060 60A60 60 0 0064 4z" />
-        <path d="M95 44H33a2 2 0 010-4h62a2 2 0 010 4zM95 66H33a2 2 0 010-4h62a2 2 0 010 4zM95 88H33a2 2 0 010-4h62a2 2 0 010 4z" />
-      </svg>
-    )
-  }
-}
+// class MenuBurger extends React.Component {
+//   render() {
+//     return (
+//       <svg
+//         onClick={this.props.menuOpen}
+//         style={{ fill: "white", cursor: "pointer" }}
+//         height="25"
+//         width="25"
+//         viewBox="0 0 128 128"
+//         xmlns="http://www.w3.org/2000/svg"
+//       >
+//         <path d="M64 128a64 64 0 1164-64 64 64 0 01-64 64zM64 4a60 60 0 1060 60A60 60 0 0064 4z" />
+//         <path d="M95 44H33a2 2 0 010-4h62a2 2 0 010 4zM95 66H33a2 2 0 010-4h62a2 2 0 010 4zM95 88H33a2 2 0 010-4h62a2 2 0 010 4z" />
+//       </svg>
+//     )
+//   }
+// }
 
 // class MenuClose extends React.Component {
 //   render() {
@@ -74,52 +74,6 @@ const MobileMenu = () => (
 )
 
 class Navbar extends React.Component {
-  render() {
-    const { fixed } = this.props
-
-    let styles = {}
-    let navClasses = "flex items-center justify-between px-2 py-3 z-40 w-full "
-
-    if (fixed) {
-      navClasses += " fixed transition ease-in-out delay-300 duration-700 "
-      if (this.state.isTop) {
-        navClasses += " bg-black bg-opacity-25 text-white "
-      } else {
-        navClasses += " bg-white shadow-md"
-        styles.opacity = 0.95
-      }
-    } else {
-      navClasses += " sticky top-0 bg-white shadow-md"
-      styles.opacity = 0.95
-    }
-
-    const pages = getPages()
-
-    let menu
-
-    if (!this.state.isMobile) {
-      menu = (
-        <ul>
-          {pages.map((page, i) => (
-            <li key={i} className="inline-block pl-5">
-              <Link to={page.link}>{page.title}</Link>
-            </li>
-          ))}
-        </ul>
-      )
-    } else {
-      menu = <MenuBurger />
-      menu = <MobileMenu />
-    }
-
-    return (
-      <nav className={navClasses} style={styles}>
-        <HomeLink />
-        {menu}
-      </nav>
-    )
-  }
-
   constructor(props) {
     super(props)
     this.state = {
@@ -154,6 +108,60 @@ class Navbar extends React.Component {
   componentWillUnmount() {
     window.removeEventListener("scroll", this.handleScroll)
     window.removeEventListener("resize", this.handleResize)
+  }
+
+  render() {
+    const { navClasses, styles } = this.getNavStyling()
+    return (
+      <nav className={navClasses} style={styles}>
+        <HomeLink />
+        {this.getMenu()}
+      </nav>
+    )
+  }
+
+  getMenu = () => {
+    const pages = getPages()
+
+    if (!this.state.isMobile) {
+      return (
+        <ul>
+          {pages.map((page, i) => (
+            <li key={i} className="inline-block pl-5">
+              <Link to={page.link}>{page.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )
+    }
+
+    return <MobileMenu />
+  }
+
+  getNavStyling = () => {
+    const navbarOpacity = 0.95
+    const { fixed } = this.props
+
+    let styles = {}
+    let navClasses = "flex items-center justify-between px-2 py-3 z-40 w-full "
+
+    if (fixed) {
+      navClasses += " fixed transition ease-in-out delay-300 duration-700 "
+      if (this.state.isTop) {
+        navClasses += " bg-black bg-opacity-25 text-white "
+      } else {
+        navClasses += " bg-white shadow-md"
+        styles.opacity = navbarOpacity
+      }
+    } else {
+      navClasses += " sticky top-0 bg-white shadow-md"
+      styles.opacity = navbarOpacity
+    }
+
+    return {
+      styles,
+      navClasses,
+    }
   }
 }
 
