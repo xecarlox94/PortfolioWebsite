f29b7de6dc021dd966b6f9fb669ab6ba4f0bd398
xecarlox94 <jf94.uk@gmail.com>
2020-05-18 10:40:48 +0100

    window problem solved

--- src/components/layouts/headers/navbar.js
+++ src/components/layouts/headers/navbar.js
@@ -3,40 +3,43 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-class MenuBurger extends React.Component() {
-  render() {
-    return (
-      <svg
-        style={{ fill: "white", cursor: "pointer" }}
-        height="25"
-        width="25"
-        viewBox="0 0 128 128"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M64 128a64 64 0 1164-64 64 64 0 01-64 64zM64 4a60 60 0 1060 60A60 60 0 0064 4z" />
-        <path d="M95 44H33a2 2 0 010-4h62a2 2 0 010 4zM95 66H33a2 2 0 010-4h62a2 2 0 010 4zM95 88H33a2 2 0 010-4h62a2 2 0 010 4z" />
-      </svg>
-    )
-  }
-}
+// class MenuBurger extends React.Component() {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+//     return (
+//       <svg
+//         style={{ fill: "white", cursor: "pointer" }}
+//         height="25"
+//         width="25"
+//         viewBox="0 0 128 128"
+//         xmlns="http://www.w3.org/2000/svg"
+//       >
+//         <path d="M64 128a64 64 0 1164-64 64 64 0 01-64 64zM64 4a60 60 0 1060 60A60 60 0 0064 4z" />
+//         <path d="M95 44H33a2 2 0 010-4h62a2 2 0 010 4zM95 66H33a2 2 0 010-4h62a2 2 0 010 4zM95 88H33a2 2 0 010-4h62a2 2 0 010 4z" />
+//       </svg>
+//     )
+//   }
+// }
 
-const MenuClose = () => (
-  <svg height="24" width="24" xmlns="http://www.w3.org/2000/svg">
-    <g fill="none" fillRule="evenodd">
-      <path
-        d="M12.5 23a11.5 11.5 0 100-23 11.5 11.5 0 000 23zm0-1a10.5 10.5 0 100-21 10.5 10.5 0 000 21zm-4.24-5.55l-.71-.7 4.24-4.25-4.24-4.24.7-.71 4.25 4.24 4.24-4.24.71.7-4.24 4.25 4.24 4.24-.7.71-4.25-4.24-4.24 4.24z"
-        fill="#000"
-      />
-    </g>
-  </svg>
-)
+// const MenuClose = () => (
+//   <svg height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+//     <g fill="none" fillRule="evenodd">
+//       <path
+//         d="M12.5 23a11.5 11.5 0 100-23 11.5 11.5 0 000 23zm0-1a10.5 10.5 0 100-21 10.5 10.5 0 000 21zm-4.24-5.55l-.71-.7 4.24-4.25-4.24-4.24.7-.71 4.25 4.24 4.24-4.24.71.7-4.24 4.25 4.24 4.24-.7.71-4.25-4.24-4.24 4.24z"
+//         fill="#000"
+//       />
+//     </g>
+//   </svg>
+// )
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isTop: this.isTop(),
-      isMobile: this.isMobile(),
+      isTop: true,
+      isMobile: false,
     }
   }
 
@@ -87,10 +90,9 @@ class Navbar extends React.Component {
         </ul>
       )
     } else {
-      menu = <MenuBurger />
+      // menu = <MenuBurger />
+      menu = <p>Burger</p>
     }
-
-    console.log("rendering")
 
     return (
       <nav className={navClasses} style={styles}>
@@ -100,11 +102,27 @@ class Navbar extends React.Component {
     )
   }
 
-  handleResize = () => this.setState({ isMobile: this.isMobile() })
+  handleResize = e => {
+    const w = e.currentTarget
+    console.log(w)
+    this.setState({
+      isMobile: w.innerWidth <= 450,
+    })
+  }
 
-  handleScroll = () => this.setState({ isTop: this.isTop() })
+  handleScroll = e => {
+    const w = e.currentTarget
+    console.log(w)
+    this.setState({
+      isTop: w.scrollY === 0,
+    })
+  }
 
   componentDidMount() {
+    this.setState({
+      isTop: window.scrollY === 0,
+      isMobile: window.innerWidth <= 450,
+    })
     window.addEventListener("resize", this.handleResize)
     window.addEventListener("scroll", this.handleScroll)
   }
@@ -113,10 +131,6 @@ class Navbar extends React.Component {
     window.removeEventListener("scroll", this.handleScroll)
     window.removeEventListener("resize", this.handleResize)
   }
-
-  isTop = () => window.scrollY === 0
-
-  isMobile = () => window.innerWidth <= 450
 }
 
 Navbar.propTypes = {
