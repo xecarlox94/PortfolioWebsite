3cef4a6b8a1b6dcfdc79b0c86221a5a0af243b4a
xecarlox94 <jf94.uk@gmail.com>
2020-05-20 22:42:57 +0100

    adding about page layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import React from "react"
 import Page from "../components/page"
 
 import Contact from "../components/fragments/contact"
+import Image from "../components/utils/image"
 
 const About = () => (
   <Page
@@ -11,7 +12,12 @@ const About = () => (
       <header className="h-screen">Welcome to the About Page!</header>
     }
   >
-    <p>main content</p>
+    <section className="grid grid-cols-1 gap-0 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+      <div>
+        <Image src="https://images.pexels.com/photos/1251299/pexels-photo-1251299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+      </div>
+      <div>description</div>
+    </section>
     <Contact />
   </Page>
 )
