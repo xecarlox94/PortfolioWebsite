0a1377ab2c7ffc1632ccf9e04ddc6fe70473f9d6
xecarlox94 <jf94.uk@gmail.com>
2020-05-21 20:58:11 +0100

    individual project page created

--- src/components/utils/image.js
+++ src/components/utils/image.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-const Image = ({ src, alt, className }) => (
-  <img src={src} alt={alt} className={className} />
+const Image = ({ src, alt, className, style }) => (
+  <img src={src} alt={alt} className={className} style={style} />
 )
 
 export default Image
--- src/pages/project.js
+++ src/pages/project.js
@@ -1,15 +1,53 @@
 import React from "react"
 
 import Page from "../components/page"
+import Image from "../components/utils/image"
 
 const Project = () => (
   <Page
     seoAttr={{ title: "Project" }}
-    headerChild={<header>Invididual Project page</header>}
+    headerChild={
+      <header
+        class="bg-header flex items-center justify-center h-screen pb-12"
+        style={{
+          background:
+            "url(https://picsum.photos/1920/1920/?random) center center no-repeat",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <div class="bg-white mx-4 p-4 text-center md:p-8">
+          <h1 class="text-5xl uppercase">Title</h1>
+        </div>
+      </header>
+    }
   >
-    <p>image</p>
-    <p>title</p>
-    <p>text</p>
+    <section className="container mx-auto max-w-5xl px-3">
+      <h2>Subtitle</h2>
+      <p>
+        The .container class sets the max-width of an element to match the
+        min-width of the current breakpoint. This is useful if you'd prefer to
+        design for a fixed set of screen sizes instead of trying to accommodate
+        a fully fluid viewport. Note that unlike containers you might have used
+        in other frameworks, Tailwind's container does not center itself
+        automatically and does not have any built-in horizontal padding.
+      </p>
+      <p>
+        The .container class sets the max-width of an element to match the
+        min-width of the current breakpoint. This is useful if you'd prefer to
+        design for a fixed set of screen sizes instead of trying to accommodate
+        a fully fluid viewport. Note that unlike containers you might have used
+        in other frameworks, Tailwind's container does not center itself
+        automatically and does not have any built-in horizontal padding.
+      </p>
+      <p>
+        The .container class sets the max-width of an element to match the
+        min-width of the current breakpoint. This is useful if you'd prefer to
+        design for a fixed set of screen sizes instead of trying to accommodate
+        a fully fluid viewport. Note that unlike containers you might have used
+        in other frameworks, Tailwind's container does not center itself
+        automatically and does not have any built-in horizontal padding.
+      </p>
+    </section>
   </Page>
 )
 
