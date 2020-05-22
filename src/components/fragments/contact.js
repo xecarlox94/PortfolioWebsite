f322946324d82dd45a235034405b95b3439dfd51
xecarlox94 <jf94.uk@gmail.com>
2020-05-22 16:09:39 +0100

    working on text input

--- src/components/fragments/contact.js
+++ src/components/fragments/contact.js
@@ -2,35 +2,49 @@ import React, { useState } from "react"
 
 class TextInput extends React.Component {
   render() {
-    const { value, inputName } = this.props
+    const { value, inputName, invalid } = this.props
     return (
       <div className="mb-4">
         <label className="block mb-2" htmlFor={inputName}>
           {inputName}
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          className={this.getInputClasses()}
           id={inputName}
           type="text"
-          placeholder={`Please insert your ${inputName}`}
+          placeholder={`Please insert your ${inputName.toLowerCase()}`}
           aria-label={inputName}
           aria-required="true"
           value={value}
           onChange={this.props.onChange}
         />
-        <p style={{ color: "red", fontStyle: "italic", paddingTop: "1vh" }}>
-          Please insert a valid {inputName}
-        </p>
+        {invalid && (
+          <p style={{ color: "red", fontStyle: "italic", paddingTop: "1vh" }}>
+            Please insert a valid {inputName.toLowerCase()}
+          </p>
+        )}
       </div>
     )
+  }
+
+  getInputClasses = () => {
+    let classes =
+      "shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline "
+
+    if (this.props.invalid) classes += " border-red-500"
+
+    return classes
   }
 }
 
 const EmailForm = () => {
   const [name, setName] = useState("")
+  let error = false
+  if (name.length === 0) error = true
   return (
     <form>
       <TextInput
+        invalid={error}
         inputName="Name"
         value={name}
         onChange={e => setName(e.target.value)}
