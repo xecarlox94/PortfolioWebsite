f9fd51e0c10206f93fdf96003f8832c8e2646677
xecarlox94 <jf94.uk@gmail.com>
2020-05-22 15:58:20 +0100

    working on text input

--- src/components/fragments/contact.js
+++ src/components/fragments/contact.js
@@ -6,20 +6,20 @@ class TextInput extends React.Component {
     return (
       <div className="mb-4">
         <label className="block mb-2" htmlFor={inputName}>
-          Name
+          {inputName}
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
           id={inputName}
           type="text"
-          placeholder="Please insert your Name"
-          aria-label="Name"
+          placeholder={`Please insert your ${inputName}`}
+          aria-label={inputName}
           aria-required="true"
           value={value}
           onChange={this.props.onChange}
         />
         <p style={{ color: "red", fontStyle: "italic", paddingTop: "1vh" }}>
-          Please insert a valid value
+          Please insert a valid {inputName}
         </p>
       </div>
     )
