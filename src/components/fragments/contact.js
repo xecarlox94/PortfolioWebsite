b1def518e358bfaa795b5ddcc319c38f007e233d
xecarlox94 <jf94.uk@gmail.com>
2020-05-22 20:52:08 +0100

    adding formik to app

--- src/components/fragments/contact.js
+++ src/components/fragments/contact.js
@@ -1,56 +1,11 @@
 import React, { useState } from "react"
 
-class TextInput extends React.Component {
-  render() {
-    const { value, inputName, invalid } = this.props
-    return (
-      <div className="mb-4">
-        <label className="block mb-2" htmlFor={inputName}>
-          {inputName}
-        </label>
-        <input
-          className={this.getInputClasses()}
-          id={inputName}
-          type="text"
-          placeholder={`Please insert your ${inputName.toLowerCase()}`}
-          aria-label={inputName}
-          aria-required="true"
-          value={value}
-          onChange={this.props.onChange}
-        />
-        {invalid && (
-          <p style={{ color: "red", fontStyle: "italic", paddingTop: "1vh" }}>
-            Please insert a valid {inputName.toLowerCase()}
-          </p>
-        )}
-      </div>
-    )
-  }
+// const Input = () => (
 
-  getInputClasses = () => {
-    let classes =
-      "shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline "
-
-    if (this.props.invalid) classes += " border-red-500"
-
-    return classes
-  }
-}
+// )
 
 const EmailForm = () => {
-  const [name, setName] = useState("")
-  let error = false
-  if (name.length === 0) error = true
-  return (
-    <form>
-      <TextInput
-        invalid={error}
-        inputName="Name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
-    </form>
-  )
+  return <form className="w-full max-w-sm"></form>
 }
 
 const Contact = () => (
