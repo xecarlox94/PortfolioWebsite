0160b28555da2f5d0ae3afe6e45a589cabb7d43d
xecarlox94 <jf94.uk@gmail.com>
2020-05-23 20:10:54 +0100

    finished with context based form

--- src/components/fragments/contact.js
+++ src/components/fragments/contact.js
@@ -2,38 +2,6 @@ import React from "react"
 import { Formik } from "formik"
 import * as Yup from "yup"
 
-// const ElementWrapper = ({ children, label }) => (
-//   <div className="md:flex md:items-center mb-6">
-//     <div className="md:w-1/3">{label ? label : null}</div>
-//     <div className="md:w-2/3">{children}</div>
-//   </div>
-// )
-
-const InputError = ({ error }) =>
-  error ? <p style={{ color: "red" }}>{error}</p> : null
-
-// const Input = props => {
-//   const { name, type, error } = props
-//   return (
-//     <ElementWrapper
-//       label={
-//         <label className="block md:text-right mb-1 md:mb-0 pr-4" htmlFor={name}>
-//           {name}
-//         </label>
-//       }
-//     >
-//       <input
-//         className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-//         placeholder={`Please insert your ${name}`}
-//         id={name}
-//         type={type}
-//         {...props}
-//       />
-//       <InputErrorMessage error={error} />
-//     </ElementWrapper>
-//   )
-// }
-
 const EmailForm = () => {
   return (
     <Formik
@@ -44,7 +12,7 @@ const EmailForm = () => {
           .required("Email is required"),
       })}
       onSubmit={values => console.log("form submitted!", values)}
-      onReset={(values, actions) => console.log("form reset")}
+      onReset={() => console.log("form reset")}
     >
       {formik => (
         <form
@@ -53,10 +21,6 @@ const EmailForm = () => {
           onReset={formik.handleReset}
         >
           <input name="email" type="email" {...formik.getFieldProps("email")} />
-          {formik.touched.email && formik.errors.email ? (
-            <InputError error={formik.errors.email} />
-          ) : null}
-          <br />
           <button type="submit">Submit</button>
           <button type="reset">Reset</button>
         </form>
@@ -73,3 +37,43 @@ const Contact = () => (
 )
 
 export default Contact
+
+// const InputError = ({ error }) =>
+//   error ? <p style={{ color: "red" }}>{error}</p> : null
+
+// const InputWrapper = ({ children, name, error }) => (
+//   <div className="md:flex md:items-center mb-6">
+//     <div className="md:w-1/3">
+//       <label className="block md:text-right mb-1 md:mb-0 pr-4" htmlFor={name}>
+//         {name}
+//       </label>
+//     </div>
+//     <div className="md:w-2/3">
+//       {children}
+//       <InputError error={error} />
+//     </div>
+//   </div>
+// )
+
+// const Input = props => {
+//   const { name, type, error } = props
+//   return (
+//     <InputWrapper name={name} error={error}>
+//       <input
+//         className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+//         placeholder={`Please insert your ${name}`}
+//         id={name}
+//         type={type}
+//         {...props}
+//       />
+//     </InputWrapper>
+//   )
+// }
+
+/* <InputError
+error={
+  formik.touched.email && formik.errors.email
+    ? formik.errors.email
+    : null
+}
+/> */
