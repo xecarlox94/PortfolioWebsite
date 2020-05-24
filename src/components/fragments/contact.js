8b99850630f6bcb5b49e54475718fd25f80a198c
xecarlox94 <jf94.uk@gmail.com>
2020-05-24 13:32:31 +0100

    adding styling to form, first 3 fields styled

--- src/components/fragments/contact.js
+++ src/components/fragments/contact.js
@@ -9,7 +9,10 @@ const validationSchema = yup.object().shape({
     .max(30, "Name must be smaller than 30 characters")
     .required("Name is required"),
   email: yup.string().email().required("Email is required"),
-  subject: yup.string().max(30, "Name must be smaller than 30 characters"),
+  subject: yup
+    .string()
+    .max(30, "Name must be smaller than 30 characters")
+    .required("Subject is required"),
   message: yup
     .string()
     .max(500, "Message must be smaller than 500 characters")
@@ -19,7 +22,32 @@ const validationSchema = yup.object().shape({
 const InputError = ({ error }) =>
   error ? <p style={{ color: "red" }}>{error.message}</p> : null
 
-// name (required), email(required), subject, message (required)
+const InputWrapper = ({ children, name, error }) => (
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block md:text-right mb-1 md:mb-0 pr-4" htmlFor={name}>
+        {name}
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      {children}
+      <InputError error={error} />
+    </div>
+  </div>
+)
+
+const Input = ({ name, error, type, register }) => (
+  <InputWrapper name={name} error={error}>
+    <input
+      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+      placeholder={`Please insert your ${name}`}
+      name={name}
+      id={name}
+      type={type}
+      ref={register}
+    />
+  </InputWrapper>
+)
 
 const ContactForm = () => {
   const { register, handleSubmit, errors } = useForm({
@@ -28,22 +56,21 @@ const ContactForm = () => {
 
   const onSubmit = data => console.log(data)
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>
-        name
-        <input type="text" name="name" ref={register} />
-        <InputError error={errors.name} />
-      </label>
-      <label>
-        email
-        <input type="email" name="email" ref={register} />
-        <InputError error={errors.email} />
-      </label>
-      <label>
-        subject
-        <input type="text" name="subject" ref={register} />
-        <InputError error={errors.subject} />
-      </label>
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-sm mx-auto">
+      <Input type="text" name="name" register={register} error={errors.name} />
+
+      <Input
+        type="email"
+        name="email"
+        register={register}
+        error={errors.email}
+      />
+      <Input
+        type="text"
+        name="subject"
+        register={register}
+        error={errors.subject}
+      />
       <label>
         textarea
         <textarea name="message" ref={register}></textarea>
@@ -62,44 +89,3 @@ const Contact = () => (
 )
 
 export default Contact
-
-// const InputWrapper = ({ children, name, error }) => (
-//   <div className="md:flex md:items-center mb-6">
-//     <div className="md:w-1/3">
-//       <label className="block md:text-right mb-1 md:mb-0 pr-4" htmlFor={name}>
-//         {name}
-//       </label>
-//     </div>
-//     <div className="md:w-2/3">
-//       {children}
-//       <InputError error={error} />
-//     </div>
-//   </div>
-// )
-
-// const Input = props => {
-//   const { name, type, error } = props
-//   return (
-//     <InputWrapper name={name} error={error}>
-//       <input
-//         className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-//         placeholder={`Please insert your ${name}`}
-//         id={name}
-//         type={type}
-//         {...props}
-//       />
-//     </InputWrapper>
-//   )
-// }
-
-/* <InputError
-error={
-  formik.touched.email && formik.errors.email
-    ? formik.errors.email
-    : null
-}
-/> */
-
-/* <label htmlFor="message">Message</label>
-<Field name="message" type="text" />
-<ErrorMessage name="message" /> */
