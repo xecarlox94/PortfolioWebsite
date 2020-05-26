978a8e6603ae043c77421804aab0ceed89f735e9
xecarlox94 <jf94.uk@gmail.com>
2020-05-26 18:49:52 +0100

    form improved

--- src/components/layouts/footers/footer.js
+++ src/components/layouts/footers/footer.js
@@ -1,10 +1,22 @@
 import React from "react"
 
+const Copyright = () => {
+  const currentDate = new Date().getFullYear()
+  return (
+    <p>
+      Copyright © {currentDate === 2020 ? currentDate : `2020 - ${currentDate}`}{" "}
+      Jose Fernandes, all rights reserved
+    </p>
+  )
+}
 const Footer = () => (
-  <footer className="h-64 bg-black text-white">
-    <h2>footer</h2>
-    <p>© {new Date().getFullYear()}</p>
+  <footer className="bg-gray-800 text-white py-10">
+    <div className="container mx-auto text-center">
+      <Copyright />
+    </div>
   </footer>
 )
+
+// https://medium.com/mojoverse-the-business-weekly/how-to-improve-google-ranking-by-optimising-website-footer-24d6b2e30e96
 
 export default Footer
