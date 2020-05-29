b7db5263232fe7c35554650ce8ca985cc18325e9
xecarlox94 <jf94.uk@gmail.com>
2020-05-29 18:45:20 +0100

    refactor navigation buttons

--- src/components/layouts/headers/mobileNav.js
+++ src/components/layouts/headers/mobileNav.js
@@ -1,44 +1,30 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
 import CloseButton from "./buttons/closeButton"
 import { getPages } from "./navbar"
 
-const MobileNav = () => {
-  useEffect(() => {
-    // constructor(props) {
-    //   super(props)
-    //   this.state = { scrollY: 0 }
-    // }
-
-    // code to disable scrolling
-    return () => {
-      // code to enable scrolling
-    }
-  }, [])
-
-  return (
-    <div
-      className="fixed top-0 z-50 h-screen px-2 py-3  w-full bg-white"
-      style={{ opacity: 0.87 }}
-    >
-      <CloseButton />
-      <nav className="flex items-center h-screen">
-        <ul className="w-full">
-          {getPages().map((page, i) => (
-            <li key={i} className="text-center my-10">
-              <Link
-                to={page.target}
-                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-              >
-                {page.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
-  )
-}
+const MobileNav = () => (
+  <div
+    className="fixed top-0 z-50 h-screen px-2 py-3  w-full bg-white"
+    style={{ opacity: 0.87 }}
+  >
+    <CloseButton />
+    <nav className="flex items-center h-screen">
+      <ul className="w-full">
+        {getPages().map((page, i) => (
+          <li key={i} className="text-center my-10">
+            <Link
+              to={page.target}
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            >
+              {page.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </div>
+)
 
 export default MobileNav
