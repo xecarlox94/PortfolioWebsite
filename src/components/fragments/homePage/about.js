20ed03c6856d1f8a8674d8faa1465bc48cb67675
xecarlox94 <jf94.uk@gmail.com>
2020-05-30 19:45:22 +0100

    about me section image improved

--- src/components/fragments/homePage/about.js
+++ src/components/fragments/homePage/about.js
@@ -1,23 +1,46 @@
 import React from "react"
 
+import { StaticQuery, graphql } from "gatsby"
+
+import Img from "gatsby-image"
+
 const About = () => (
-  <section className="my-40 px-4">
-    <h1 className="text-center mb-20">Who am I?</h1>
-    {/* <Img
-      className="rounded-full mx-auto"
-      src="https://tailwindcss.com/img/jonathan.jpg"
-      alt="Jose Fernandes"
-      style={{
-        minHeight: "10rem",
-        minWidth: "10rem",
-        height: "33%",
-        width: "33%",
-      }}
-    /> */}
-    <div className="text-center">
-      <p>Description</p>
-    </div>
-  </section>
+  <StaticQuery
+    query={graphql`
+      query MyQuery {
+        imageSharp(fixed: { originalName: { eq: "portrait.jpeg" } }) {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    `}
+    render={({ imageSharp: { fluid } }) => (
+      <section className="my-40 px-4">
+        <h1 className="text-center mb-20" style={{}}>
+          Who am I?
+        </h1>
+        <Img
+          className="mx-auto "
+          fluid={fluid}
+          alt="Jose Fernandes"
+          style={{
+            minHeight: "10rem",
+            minWidth: "10rem",
+            height: "33%",
+            width: "33%",
+          }}
+          imgStyle={{
+            objectFit: "contain",
+            borderRadius: "1rem",
+          }}
+        />
+        <div className="text-center">
+          <p>Description</p>
+        </div>
+      </section>
+    )}
+  />
 )
 
 export default About
